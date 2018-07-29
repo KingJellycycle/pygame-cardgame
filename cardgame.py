620286a45ac176ecb965a99349d5bddf14204ada
King Jellycycle <UnfeelingTiger506@gmail.com>
2018-07-29 17:12:24 +1000

    Update - Added Art and Stuff

--- cardgame.py
+++ cardgame.py
@@ -21,14 +21,14 @@ class cardgame:
         self.cards = [
             # Champions
             [#self.Summon(table,self.cards[0][1])
-                ["Azir","Summons 3 soldiers",2,2,1],
-                ["Morgana","Nullifies status effects on unit (pick)",1,3,1],
+                ["Azir","Summons 3 soldiers",2,2,3],
+                ["Morgana","Nullifies satus effects on unit (pick)",1,3,3],
                 ["Ashe","Freeze selected unit",4,1,1],
-                ["Jarven IV","Adjacent allies gain 2 atk",2,2,1],
-                ["Kindred","Randomly marks a unit on the border, killing this unit gives +3 atk",3,2,1],
+                ["Jarven IV","Adjacent allies gain 2 atk",2,2,4],
+                ["Kindred","Randomly marks a unit on the border, killing this unit gives +3 atk",2,2,4],
                 ["Minion (Melee)","Basic Unit",1,3,1],
-                ["Super Minion","Basic Unit",3,3,1],
-                ["Minion (Caster)","Basic Unit",4,1,1]
+                ["Super Minion","Basic Unit",3,3,3],
+                ["Minion (Caster)","Basic Unit",4,1,2]
             ],
             # Buffs/Potions/Items
             [
